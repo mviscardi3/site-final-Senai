d2ee4f1f1aaf8b712fd8620f7f2af72289bd40ee
mviscardi3 <miguelviscardi@gmail.com>
2025-04-03 20:11:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let cartCount = 0;
-document.querySelectorAll('.buy-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        cartCount++;
-        document.getElementById('cart-count').innerText = cartCount;
-    });
-});
